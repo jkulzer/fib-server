575c4a898be6f51e80d21472517b042c4c8d9aea
jkulzer <git@jkulzer.dev>
2025-01-11 02:17:44 +0100

    updated shared models

--- sharedModels/models.go
+++ sharedModels/models.go
@@ -1,6 +1,16 @@
 package sharedModels
 
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
 type LoginInfo struct {
 	Username string
 	Password string
+}
+
+type SessionToken struct {
+	Token  uuid.UUID
+	Expiry time.Time
 }
